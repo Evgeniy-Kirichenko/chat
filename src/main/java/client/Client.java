c6cfbcd941222f88fa4fef7b7a714fe973ebbc16
Evgeni Kirichenko <avikon3412@yandex.ru>
2023-02-18 08:42:24 +0300

    убрал комментарии

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -15,45 +15,6 @@ public class Client {
     private BufferedReader br;
     private PrintWriter bw;
 
-//    public Client() {
-//    }
-
-    //    public Client() {
-//        final Config config = Config.getInstance();
-//        try {
-//            sc = new Scanner(System.in);
-//            socket = new Socket(config.getIp(), config.getPort());
-//            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//            bw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);//делаем autoFlush
-//            System.out.println("Введите свое имя");
-//            String name = sc.nextLine();//читаем имя пользователя
-//            bw.println(name); // отправляем имя пользователя в socket
-//
-//            ListerServer listerServer = new ListerServer(br);//создаем отдельный поток для приема сообщений с сервера
-//            listerServer.start();// запускаем поток
-//
-//            String msg = "";
-//            while (!"exit".equalsIgnoreCase(msg)) {
-//                msg = sc.nextLine();//пишем свое сообщение
-//                bw.println(msg);//отправляем сообщение на сервер
-//            }
-//
-//            listerServer.interrupt();//запрос на прерывание потока сообщений с сервера
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                sc.close();
-//                socket.close();
-//                bw.close();
-//                br.close();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//        }
-//    }
     public void startClient(){
         final Config config = Config.getInstance();
         try {
